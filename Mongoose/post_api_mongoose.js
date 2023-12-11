74ec5e496bef739e01201d308c783530199ad4c0
Saumya <saumya.shikhar@ens.enterprises>
2023-12-11 13:10:15 +0530

    push

--- Mongoose/post_api_mongoose.js
+++ Mongoose/post_api_mongoose.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose');
+const productSch = new mongoose.Schema(
+    {
+        brand:String,
+        name:String,
+        price:Number
+    }
+);
+
+const productmodel = mongoose.model('products',productSch)
+
